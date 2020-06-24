6e5b7065d1d51c0c86f89b4a71e69270bab7a2a2
tredok <wrk.dodge@gmail.com>
2020-06-24 22:25:21 +0300

    step 5

---
+++ src/js/modules/forms.js
@@ -0,0 +1,60 @@
+import { post } from "jquery"
+
+const forms = () => {
+    const form = document.querySelectorAll('form')
+    const inputs = document.querySelectorAll('input')
+    const inputsTelephone = document.querySelectorAll('input[name="user_phone"]')
+    
+    inputsTelephone.forEach(item => {
+        item.addEventListener('input', (evt) => {
+            item.value = item.value.replace(/\D/, '')
+        })
+    })
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, с вами скоро свяжутся',
+        failure: 'Что-то пошло не так...'
+    }
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading
+        const result = await fetch(url, {
+            method: 'POST',
+            body: data
+        })
+        return await result.text()
+    }
+
+    form.forEach(item => {
+        item.addEventListener('submit', (evt) => {
+            evt.preventDefault()
+            const statusMessage = document.createElement('div')
+            item.append(statusMessage)
+
+            const formData = new FormData(item)
+
+            postData('', formData)
+                .then(result => {
+                    console.log(result)
+                    statusMessage.textContent = message.success
+                }).catch(() => {
+                    console.log('failed')
+                    statusMessage.textContent = message.failure
+                }).finally(() => {
+                    clearInputs()
+                    setTimeout(() => {
+                        statusMessage.remove()
+                    }, 5000)
+                })
+        })
+    })
+
+    const clearInputs = () => {
+        inputs.forEach(input => {
+            input.value = ''
+        })
+    }
+}
+
+export default forms
